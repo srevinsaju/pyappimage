404c8a046cb0ab99a779281068341ad1da411e6a
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-20 21:05:26 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from zap import __version__
+from setuptools import find_packages
+from setuptools import setup
+
+
+CLASSIFIERS = '''\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+Programming Language :: Python
+Topic :: Software Development
+Operating System :: POSIX :: Linux
+'''
+
+
+setup(
+    name='pyappimage',
+    version=__version__,
+    packages=find_packages(),
+    url='https://github.com/srevinsaju/pyappimage',
+    license='MIT',
+    author='srevinsaju',
+    author_email='srevinsaju@sugarlabs.org',
+    description='Python Appimage builder',
+    project_urls={
+        'Bug Tracker': 'https://github.com/srevinsaju/pyappimage/issues',
+        'Source Code': 'https://github.com/srevinsaju/pyappimage',
+    },
+    platforms=['Linux'],
+    include_package_data=True,
+    install_requires=['PyInstaller', 'click', 'halo'],
+    dependency_links=[
+        "http://github.com/srevinsaju/zap/archive/master.tar.gz"
+    ],
+    python_requires='>=3.4',
+    entry_points={
+        'console_scripts': (
+            'pyappimage = pyappimage.cli:cli',
+        )
+    },
+    classifiers=[s for s in CLASSIFIERS.split(os.linesep) if s.strip()],
+)
