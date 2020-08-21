52e9a202293ec7d0657bcef2f04bc15e17e4ed56
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-21 23:54:10 +0300

    Add libpython*.m.so to /usr/lib

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,3 +1,3 @@
 export PYAPPIMAGE_PIP=${APPDIR}/opt/python{{ python-version }}/bin/pip
-export LD_LIBRARY_PATH=${APPDIR}/opt/python{{ python-version }}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${APPDIR}/opt/python{{ python-version }}/lib:${APPDIR}/usr/lib:$LD_LIBRARY_PATH
 {{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/pyappimage "$@"
