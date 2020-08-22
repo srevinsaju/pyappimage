bb184d4de0a10547548eccf0a5576478fb411a35
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-22 16:36:13 +0300

    Bundle objdump and strip from binutils (ubuntu)

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,3 +1,4 @@
+export PATH=${APPDIR}/usr/bin:$PATH
 export PYAPPIMAGE_PIP=${APPDIR}/opt/python{{ python-version }}/bin/pip
 export LD_LIBRARY_PATH=${APPDIR}/opt/python{{ python-version }}/lib:${APPDIR}/usr/lib:$LD_LIBRARY_PATH
 {{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/pyappimage "$@"
